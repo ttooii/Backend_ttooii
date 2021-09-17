4aeb574273cdefe793a83553c48242b256c650a6
jjyu <79048655+jjyu9312@users.noreply.github.com>
2021-09-17 21:00:31 +0900

    first commit
    09/17
    기본 환경 구성

---
+++ src/main/java/com/toyproject/realty/RealtyApplication.java
@@ -0,0 +1,13 @@
+package com.toyproject.realty;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RealtyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RealtyApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/toyproject/realty/RealtyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.toyproject.realty;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RealtyApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
