b4d257ce12ad59f05f7133b68ed2e28b67ad1b49
oeunz <oeunzzang@naver.com>
2021-10-11 05:03:38 +0900

    10/11

--- src/main/java/com/toyproject/realty/controller/UserController.java
+++ src/main/java/com/toyproject/realty/controller/UserController.java
@@ -6,7 +6,7 @@
 //import org.springframework.stereotype.Controller;
 //import org.springframework.web.bind.annotation.GetMapping;
 //import org.springframework.web.bind.annotation.PostMapping;
-//@Api(tags = {"로컬 로그인 API"})
+//
 //@Controller
 //@AllArgsConstructor
 //public class UserController {
