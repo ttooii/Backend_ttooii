9b7889874aa369fa2a3cf99e387a2a17c4b31347
oeunz <oeunzzang@naver.com>
2021-10-13 14:34:36 +0900

    10/13

--- src/main/java/com/toyproject/realty/controller/MainController.java
+++ src/main/java/com/toyproject/realty/controller/MainController.java
@@ -1,5 +1,6 @@
 package com.toyproject.realty.controller;
 
+import io.swagger.annotations.ApiOperation;
 import io.swagger.annotations.ApiParam;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,7 +8,7 @@ import org.springframework.web.bind.annotation.RestController;
 public class MainController {
 
     // 메인 페이지
-    @ApiParam(value = "메인페이지")
+    @ApiOperation(value = "메인페이지")
     @GetMapping("/")
     public String index() {
         return "/index";
