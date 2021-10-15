84cf27887e3b1ea5fd6d274e1d7f050557e6e5d3
jjyu <79048655+jjyu9312@users.noreply.github.com>
2021-10-15 16:07:02 +0900

    21/10/15 [error] : Error creating bean with name 'entityManagerFactory' defined in class path resource <br> -> jpa properties 수정

--- src/main/java/com/toyproject/realty/controller/OAuth2Controller.java
+++ src/main/java/com/toyproject/realty/controller/OAuth2Controller.java
@@ -13,18 +13,21 @@ public class OAuth2Controller {
 
     @GetMapping({"", "/"})
     public String getAuthorizationMessage() {
-        return "home";
+        return "index";
     }
+
     @ApiOperation(value = "소셜로그인")
     @GetMapping("/login")
     public String login() {
         return "login";
     }
+
     @ApiParam(value = "소셜로그인성공")
     @GetMapping({"/loginSuccess", "/loginSuccess"})
     public String loginSuccess() {
         return "loginSuccess";
     }
+
     @ApiParam(value = "소셜로그인실패")
     @GetMapping("/loginFailure")
     public String loginFailure() {
--- src/main/java/com/toyproject/realty/dto/MemberDto.java
+++ src/main/java/com/toyproject/realty/dto/MemberDto.java
@@ -16,15 +16,15 @@ import java.time.LocalDateTime;
 @NoArgsConstructor
 public class MemberDto {
     @NotBlank(message = "ID는 필수 입력 값입니다.")
-    @Size(max=20, message = "20자 이하로 입력해주세요.")
+    @Size(max = 20, message = "20자 이하로 입력해주세요.")
     private String userId;
 
     @NotBlank(message = "Name은 필수 입력 값입니다. ")
-    @Size(max=50, message = "50자 이하로 입력해주세요.")
+    @Size(max = 50, message = "50자 이하로 입력해주세요.")
     private String username;
 
     @NotBlank(message = "Email은 필수 입력 값입니다.")
-    @Size(max=50, message = "50자 이하로 입력해주세요.")
+    @Size(max = 50, message = "50자 이하로 입력해주세요.")
     @Email(message = "이메일 형식에 맞지 않습니다.")
     private String email;
 
@@ -32,13 +32,13 @@ public class MemberDto {
     private String phone;
 
     @NotBlank(message = "Password는 필수 입력 값입니다. 20자 이하로 입력해주세요.")
-    @Size(max=20, message = "20자 이하로 입력해주세요.")
+    @Size(max = 20, message = "20자 이하로 입력해주세요.")
     private String password;
 
     private LocalDateTime createdDate;
     private LocalDateTime modifiedDate;
 
-    public Member toEntity(){
+    public Member toEntity() {
         return Member.builder()
                 .userId(userId)
                 .username(username)
@@ -49,7 +49,7 @@ public class MemberDto {
     }
 
     @Builder
-    public MemberDto(String userId,String email, String username, String phone, String password){
+    public MemberDto(String userId, String email, String username, String phone, String password) {
         this.userId = userId;
         this.email = email;
         this.username = username;
@@ -59,8 +59,4 @@ public class MemberDto {
 
     public void setPassword() {
     }
-<<<<<<< HEAD
 }
-=======
-}
->>>>>>> master
