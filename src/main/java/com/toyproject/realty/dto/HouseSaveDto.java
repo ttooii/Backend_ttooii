bca05a974e5a2026ef0eecf0bb764910c69021c0
oeunz <oeunzzang@naver.com>
2021-10-18 03:55:42 +0900

    10/18

---
+++ src/main/java/com/toyproject/realty/dto/HouseSaveDto.java
@@ -0,0 +1,69 @@
+package com.toyproject.realty.dto;
+
+import com.toyproject.realty.entity.House;
+import lombok.Builder;
+
+public class HouseSaveDto {
+
+    private Long houseId;
+    private String transactionType;
+    private Float exclusiveArea;
+    private int floor;
+    private int totalFloor;
+    private int marketValue;
+    private int roomCount;
+    private int administrationCost;
+    private int bathroomCount;
+    private String direction;
+    private String heatingSystem;
+    private String title;
+    private String content;
+    private int landArea;
+    private int parkingCount;
+    private String purpose;
+    private int confirmation;
+
+
+    public House toEntity() {
+        return House.builder()
+                .houseId(houseId)
+                .transactionType(transactionType)
+                .exclusiveArea(exclusiveArea)
+                .floor(floor)
+                .totalFloor(totalFloor)
+                .marketValue(marketValue)
+                .roomCount(roomCount)
+                .administrationCost(administrationCost)
+                .bathroomCount(bathroomCount)
+                .direction(direction)
+                .heatingSystem(heatingSystem)
+                .title(title)
+                .content(content)
+                .landArea(landArea)
+                .parkingCount(parkingCount)
+                .purpose(purpose)
+                .confirmation(confirmation)
+                .build();
+    }
+
+    @Builder
+    public HouseSaveDto() {
+        this.houseId = houseId;
+        this.transactionType = transactionType;
+        this.exclusiveArea = exclusiveArea;
+        this.floor = floor;
+        this.totalFloor = totalFloor;
+        this.marketValue = marketValue;
+        this.roomCount = roomCount;
+        this.administrationCost = administrationCost;
+        this.bathroomCount = bathroomCount;
+        this.direction = direction;
+        this.heatingSystem = heatingSystem;
+        this.title = title;
+        this.content = content;
+        this.landArea = landArea;
+        this.parkingCount = parkingCount;
+        this.purpose = purpose;
+        this.confirmation = confirmation;
+    }
+}
