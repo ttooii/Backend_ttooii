e163d9185bbde8ce4817341f0b4a5ec4fc75b2bd
jjyu <79048655+jjyu9312@users.noreply.github.com>
2021-11-23 00:03:53 +0900

    21/11/21 - 전체 merge \n - 프로젝트 명 변경

--- src/main/java/com/toyproject/Backend_ttooii/repository/MemberRepository.java
+++ src/main/java/com/toyproject/Backend_ttooii/repository/MemberRepository.java
@@ -8,4 +8,6 @@ import java.util.Optional;
 
 public interface MemberRepository extends JpaRepository<Member, String> {
     Optional<Member> findByuserId(String userId);
+
+    Optional<Member> findByEmail(String email);
 }
