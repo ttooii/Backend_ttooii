7a0ef107718d72b0f254faad890f18353b90f5cc
YoungEunKim99 <56907015+YoungEunKim99@users.noreply.github.com>
2021-12-14 22:03:48 +0900

    Delete src/main/java/com/toyproject/realty directory

--- src/main/java/com/toyproject/realty/repository/BoardRepository.java
+++
@@ -1,7 +0,0 @@
-package com.toyproject.realty.repository;
-
-import com.toyproject.realty.entity.Board;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface BoardRepository extends JpaRepository<Board,Long> {
-}
